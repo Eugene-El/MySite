c5566b5d2034beae8c20b35f531e7bd0f91fadf9
El <lomashonok@inbox.lv>
2018-03-05 21:54:59 +0200

    Weater

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ var handlebars = require('express-handlebars').create({
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.disable('x-powered-by');
+
 var randomError = require('./lib/randomError.js');
 
 app.set('port', process.env.PORT || 3000);
@@ -28,12 +30,24 @@ app.use(function(req, res, next){
 	next();
 });
 
+app.use(function(req, res, next) {
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weatherContext = getWeatherData();
+	next();
+});
 
 // References
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	res.render('home');
 });
-app.get('/about', function(req, res){
+app.get('/headers', function(req, res) {
+	res.set('Content-Type', 'text/plain');
+	var s = '';
+	for(var name in req.headers)
+		s += name + ': ' + req.headers[name] + '\n';
+	res.send(s);
+});
+app.get('/about', function(req, res) {
 	res.render('about', {
 		pageTestScript: '/qa/tests-about.js'
 	});
@@ -46,8 +60,11 @@ app.get('/projects/request-group', function(req, res) {
 });
 
 
+
+
+
 // 404 error page
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
 	res.status(404);
 	res.render('404', { 
 		errorMessage: randomError.getRandomError() 
@@ -55,14 +72,32 @@ app.use(function(req, res, next){
 });
 
 // 500 error page
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function() {
 	console.log('Server started on http://localhost:' + app.get('port'));
 });
 
 
+function getWeatherData() {
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastUrl: 'http://wunderground.com/US/OR/Portland.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				temp: '54.1 F (12.3 C)'
+			},
+			{
+				name: 'Bend',
+				forecastUrl: 'http://wunderground.com/US/OR/Bend.html',
+				iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				temp: '55.0 F (12.8 C)'
+			}
+		]
+	};
+}
